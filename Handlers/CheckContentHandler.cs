1f4aefe31b156908f83c449682c5e86d82c8a7ea
SwayInCollaborate <sway.chen@collaborate.tw>
2020-12-29 12:23:28 +0800

    檢查是否big5跟utf8的字幕內容是否一致，如一致，可刪除

---
+++ Handlers/CheckContentHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SaveUtf8Srt.Handlers
+{
+    public class CheckContentHandler : DirRecursiveHandler
+    {
+        private readonly IList<(string path, string oriFileName)> OriFilePaths = new List<(string path, string oriFileName)>();
+
+        protected override void BeforeRecusiveSearch(string path)
+        {
+            OriFilePaths.Clear();
+        }
+
+        protected override void ProcessFiles(string path, IEnumerable<string> files)
+        {
+            if (!files.Any(x => x.EndsWith(".srt")) || !files.Any(x => x.EndsWith(".original.srt")))
+                return;
+
+            var oriSrtFile = files.First(x => x.EndsWith(".original.srt"));
+            var oriSrt = GetContents(oriSrtFile);
+            var srtFile = files.First(x => !x.Equals(oriSrtFile) && x.EndsWith(".srt"));
+            var srt = GetContents(srtFile);
+
+            if (oriSrt.Equals(srt, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var name = Path.GetFileNameWithoutExtension(oriSrtFile);
+                Console.WriteLine($"{name.Replace(".original", string.Empty)} is the same");
+                OriFilePaths.Add((path, name));
+            }
+        }
+
+        private string GetContents(string filePath)
+        {
+            var encoding = StrHelper.GetEncoding(filePath);
+            var s = string.Empty;
+            using (var sr = new StreamReader(filePath, encoding, true))
+            {
+                s = sr.ReadToEnd();
+            }
+            return s;
+        }
+
+        protected override void AfterRecusiveSearch(string path)
+        {
+            if (OriFilePaths.Count == 0)
+                return;
+
+            Console.WriteLine("Delete original file?");
+            var command = Console.ReadLine();
+            if (!string.Equals(command, "Y", StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+            foreach (var (oripath, oriFileName) in OriFilePaths)
+            {
+                var filePath = Path.Combine(oripath, oriFileName + ".srt");
+                File.Delete(filePath);
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
--- Handlers/SrtHandler.cs
+++ Handlers/SrtHandler.cs
@@ -1,56 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SaveUtf8Srt.Handlers
 {
-    public class SrtHandler
+    public class SrtHandler : DirRecursiveHandler
     {
-        public void Execute(string path)
+        protected override void ProcessFiles(string path, IEnumerable<string> files)
         {
-            if (string.IsNullOrEmpty(path) || !Directory.Exists(path) || path.EndsWith("System Volume Information"))
-                return;
-
-            var (dirs, isAccessable) = GetEnumerateDirectories(path);
-            if (!isAccessable)
-                return;
-            if (dirs.Any())
-            {
-                foreach (var dir in Directory.EnumerateDirectories(path))
-                {
-                    Execute(dir);
-                }
-            }
-            else
-            {
-                CheckSrt(path);
-            }
-        }
-
-        private (IEnumerable<string> dirs, bool isAccessable) GetEnumerateDirectories(string path)
-        {
-            try
-            { 
-                return (Directory.EnumerateDirectories(path), true);
-            }
-            catch
-            {
-                return (Enumerable.Empty<string>(), false);
-            }
-        }
-
-        private void CheckSrt(string path)
-        {
-            var srts = Directory.EnumerateFiles(path).Where(x => x.EndsWith(".srt"));
+            var srts = files.Where(x => x.EndsWith(".srt"));
             if (!srts.Any())
                 return;
 
             foreach (var srt in srts)
             {
-                var encoding = GetEncoding(srt);
+                var encoding = StrHelper.GetEncoding(srt);
                 if (encoding == Encoding.UTF8 || encoding == Encoding.Unicode)
                     continue;
 
@@ -67,28 +32,6 @@ namespace SaveUtf8Srt.Handlers
                 File.Move(srt, newPath);
                 File.WriteAllText(srt, s, Encoding.UTF8);
             }
-        }
-
-        private Encoding GetEncoding(string filename)
-        {
-            // Read the BOM
-            var bom = new byte[4];
-            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                file.Read(bom, 0, 4);
-            }
-
-            // Analyze the BOM
-            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
-            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
-            if (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) return Encoding.UTF32; //UTF-32LE
-            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
-            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
-            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return new UTF32Encoding(true, true);  //UTF-32BE
-
-            // We actually have no idea what the encoding is if we reach this point, so
-            // you may wish to return null instead of defaulting to ASCII
-            return Encoding.Default;
         }
     }
 }
